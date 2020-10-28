1139b66abc88e119b9b1b450f95e15039ccbd128
sumancfc <sumanstha999@gmail.com>
2020-10-28 10:48:34 +0545

    Js added

--- apple-header/main.js
+++ apple-header/main.js
@@ -1,1 +1,15 @@
+$(document).ready(function () {
+  $("#search").click(function () {
+    $(".menu__item").addClass("hide__item");
+    $(".search__form").addClass("active");
+    $(".close").addClass("active");
+    $("#search").hide();
+  });
 
+  $(".close").click(function () {
+    $(".menu__item").removeClass("hide__item");
+    $(".search__form").removeClass("active");
+    $(".close").removeClass("active");
+    $("#search").show();
+  });
+});
